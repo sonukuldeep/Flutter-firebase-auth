6bd94680acd4fda551b801c6807d1dd7e65e5f93
sonukuldeep <sonukuldeep@outlook.com>
2023-09-29 13:18:08 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/pages/login.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp(userRepository: FirebaseUserRepo()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final UserRepository userRepository;
+  const MyApp({super.key, required this.userRepository});
 
   @override
   Widget build(BuildContext context) {
